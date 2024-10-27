bdb243c8a160ab6a08c5c2625346496e445209b2
zprl <zprl.dev@gmail.com>
2024-10-27 22:30:21 +0900

    fix

--- src/game/plugin.rs
+++ src/game/plugin.rs
@@ -2,14 +2,14 @@ use bevy::prelude::*;
 
 use super::{
   player::plugin::PlayerPlugin, shader::plugin::ShaderPlugin, test::plugin::TestPlugin,
-  ui::plugin::UiPlugin,
+  ui::plugin::UiPlugin, world::plugin::WorldPlugin,
 };
 
 pub struct GamePlugin;
 
 impl Plugin for GamePlugin {
   fn build(&self, app: &mut bevy::prelude::App) {
-    let app = app.add_plugins((UiPlugin, PlayerPlugin, ShaderPlugin));
+    let app = app.add_plugins((PlayerPlugin, ShaderPlugin, UiPlugin, WorldPlugin));
 
     if cfg!(debug_assertions) {
       app.add_plugins(TestPlugin);
