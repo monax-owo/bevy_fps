fcde960f069a76dc1fe632c52c69698e8b2c7f72
zprl <zprl.dev@gmail.com>
2024-10-27 23:27:38 +0900

    remove FrameTimeDiagnosticsPlugin

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,12 @@
 pub mod game;
 
-use bevy::{diagnostic::FrameTimeDiagnosticsPlugin, prelude::*};
+use bevy::prelude::*;
 use game::plugin::GamePlugin;
 // use bevy_rapier3d::prelude::*;
 
 fn main() {
   let mut app = App::new();
-  app.add_plugins((
-    DefaultPlugins,
-    FrameTimeDiagnosticsPlugin::default(),
-    GamePlugin,
-  ));
+  app.add_plugins((DefaultPlugins, GamePlugin));
 
   if true {
     use bevy_editor_pls::EditorPlugin;
