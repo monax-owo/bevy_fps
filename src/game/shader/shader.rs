0ab6b4eba23f88b30a46b68f81117b907b2afe77
zprl <zprl.dev@gmail.com>
2024-10-28 12:53:17 +0900

    fix

--- src/game/shader/shader.rs
+++ src/game/shader/shader.rs
@@ -1,22 +1,13 @@
 use bevy::prelude::*;
 use bevy::render::render_resource::AsBindGroup;
 
-#[derive(Clone, Asset, AsBindGroup, TypePath)]
+#[derive(Clone, Default, Asset, AsBindGroup, TypePath)]
 pub struct PostProcessMaterial {
   #[uniform(0)]
   pub color: LinearRgba,
   #[texture(1)]
   #[sampler(2)]
   pub color_texture: Option<Handle<Image>>,
-}
-
-impl Default for PostProcessMaterial {
-  fn default() -> Self {
-    Self {
-      color: Default::default(),
-      color_texture: Default::default(),
-    }
-  }
 }
 
 // TODO:<https://bevyengine.org/examples/2d-rendering/pixel-grid-snap/>これでよくない？試します
