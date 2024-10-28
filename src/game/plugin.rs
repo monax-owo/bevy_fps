abc7d3a028ee9bf408079e4d6510eb8dca45bf5e
zprl <zprl.dev@gmail.com>
2024-10-28 12:24:58 +0900

    add rapier plugins

--- src/game/plugin.rs
+++ src/game/plugin.rs
@@ -1,4 +1,8 @@
 use bevy::prelude::*;
+use bevy_rapier3d::{
+  plugin::{NoUserData, RapierPhysicsPlugin},
+  render::RapierDebugRenderPlugin,
+};
 
 use super::{
   player::plugin::PlayerPlugin, shader::plugin::ShaderPlugin, test::plugin::TestPlugin,
@@ -9,7 +13,14 @@ pub struct GamePlugin;
 
 impl Plugin for GamePlugin {
   fn build(&self, app: &mut bevy::prelude::App) {
-    let app = app.add_plugins((PlayerPlugin, ShaderPlugin, UiPlugin, WorldPlugin));
+    let app = app.add_plugins((
+      RapierPhysicsPlugin::<NoUserData>::default(),
+      RapierDebugRenderPlugin::default(),
+      PlayerPlugin,
+      ShaderPlugin,
+      UiPlugin,
+      WorldPlugin,
+    ));
 
     if cfg!(debug_assertions) {
       app.add_plugins(TestPlugin);
