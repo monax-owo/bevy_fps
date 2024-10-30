2e3edb3e6129fa7f5e4fcb5397dd6f012817b986
zprl <zprl.dev@gmail.com>
2024-10-30 23:15:33 +0900

    fix grounded

--- src/game/player/movement.rs
+++ src/game/player/movement.rs
@@ -6,16 +6,10 @@ use super::player::Player;
 pub(super) fn update_movement(
   key: Res<ButtonInput<KeyCode>>,
   time: Res<Time>,
-  mut query: Query<(
-    &mut Player,
-    &Transform,
-    &mut KinematicCharacterController,
-    Option<&KinematicCharacterControllerOutput>,
-  )>,
+  rapier_context: Res<RapierContext>,
+  mut player_query: Query<(&mut Player, &Transform, &mut KinematicCharacterController)>,
 ) {
-  if let Ok((mut player, player_transform, mut controller, controller_output)) =
-    query.get_single_mut()
-  {
+  if let Ok((mut player, player_transform, mut controller)) = player_query.get_single_mut() {
     // TODO:directionをVec2,重力を別の変数にする
     // Vec3(x,y,z) Vec2(x,z)
     let mut horizontal_direction = Vec2::ZERO;
@@ -38,19 +32,33 @@ pub(super) fn update_movement(
 
     horizontal_direction = horizontal_direction.clamp_length(0.0, 1.0) * player.horizontal_speed;
 
-    if let Some(controller_output) = controller_output {
-      // 地面に付いて無いときは重力を加える
-      if controller_output.grounded {
-        player.gravity = 0.0;
-        if key.pressed(KeyCode::Space) {
-          player.gravity = -32.0;
-        }
-      } else {
-        player.gravity += 5.0 * player.vertical_speed * time.delta_seconds();
-      }
+    // 地面に付いて無いときは重力を加える
+    // TODO:接地判定を変える
+    let grounded = rapier_context
+      .cast_ray(
+        Vec3::new(
+          player_transform.translation.x,
+          player_transform.translation.y - 1.4,
+          player_transform.translation.z,
+        ),
+        -Vec3::Y,
+        0.02,
+        true,
+        QueryFilter::exclude_kinematic(),
+      )
+      .is_some();
+    // println!("{}", grounded);
 
-      player.direction.y -= player.gravity * 0.2;
+    if grounded {
+      player.gravity = 0.0;
+      if key.pressed(KeyCode::Space) {
+        player.gravity = -32.0;
+      }
+    } else {
+      player.gravity += 5.0 * player.vertical_speed * time.delta_seconds();
     }
+
+    player.direction.y -= player.gravity * 0.2;
 
     player.direction = Vec3::from((
       horizontal_direction.x,
@@ -58,6 +66,7 @@ pub(super) fn update_movement(
       horizontal_direction.y,
     )) * time.delta_seconds();
 
+    // TODO:translationじゃなくてvelocityを使う？
     controller.translation = Some(player.direction);
   }
 }
