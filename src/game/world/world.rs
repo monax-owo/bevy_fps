9bc160e946d3f876e2558833495f4b09979fd508
zprl <zprl.dev@gmail.com>
2024-10-31 09:44:51 +0900

    grouping

--- src/game/world/world.rs
+++ src/game/world/world.rs
@@ -15,62 +15,65 @@ pub(super) fn init_world(
   });
 
   // Ground
-  commands.spawn((
-    Collider::cuboid(100.0, 0.01, 100.0),
-    PbrBundle {
-      mesh: meshes.add(Plane3d::new(Vec3::Y, Vec2::splat(100.0))),
-      material: materials.add(Color::srgb_u8(255, 0, 127)),
-      ..default()
-    },
-  ));
+  commands
+    .spawn((
+      Name::new("World"),
+      Collider::cuboid(100.0, 0.01, 100.0),
+      PbrBundle {
+        mesh: meshes.add(Plane3d::new(Vec3::Y, Vec2::splat(100.0))),
+        material: materials.add(Color::srgb_u8(255, 0, 127)),
+        ..default()
+      },
+    ))
+    .with_children(|parent| {
+      // Box
+      parent.spawn((
+        Collider::cuboid(1.0, 1.0, 1.0),
+        RigidBody::Dynamic,
+        PbrBundle {
+          mesh: meshes.add(Cuboid::new(2.0, 2.0, 2.0)),
+          material: materials.add(Color::srgb_u8(255, 0, 0)),
+          transform: Transform::from_xyz(0.0, 4.0, -4.0),
+          ..default()
+        },
+      ));
 
-  // Box
-  commands.spawn((
-    Collider::cuboid(1.0, 1.0, 1.0),
-    RigidBody::Dynamic,
-    PbrBundle {
-      mesh: meshes.add(Cuboid::new(2.0, 2.0, 2.0)),
-      material: materials.add(Color::srgb_u8(255, 0, 0)),
-      transform: Transform::from_xyz(0.0, 4.0, -4.0),
-      ..default()
-    },
-  ));
+      // Gizmo
+      parent.spawn(PbrBundle {
+        mesh: meshes.add(Rhombus {
+          half_diagonals: Vec2::splat(0.1),
+        }),
+        material: materials.add(Color::srgb_u8(255, 0, 0)),
+        ..default()
+      });
 
-  // Gizmo
-  commands.spawn(PbrBundle {
-    mesh: meshes.add(Rhombus {
-      half_diagonals: Vec2::splat(0.1),
-    }),
-    material: materials.add(Color::srgb_u8(255, 0, 0)),
-    ..default()
-  });
+      // Sphere top
+      parent.spawn(PbrBundle {
+        mesh: meshes.add(Sphere::new(SPHERE_SIZE)),
+        material: materials.add(Color::srgb_u8(255, 0, 0)),
+        transform: Transform::from_xyz(0.0, 4.0, 0.0),
+        ..default()
+      });
 
-  // Sphere top
-  commands.spawn(PbrBundle {
-    mesh: meshes.add(Sphere::new(SPHERE_SIZE)),
-    material: materials.add(Color::srgb_u8(255, 0, 0)),
-    transform: Transform::from_xyz(0.0, 4.0, 0.0),
-    ..default()
-  });
+      // Sphere forward
+      parent.spawn(PbrBundle {
+        mesh: meshes.add(Sphere::new(SPHERE_SIZE)),
+        material: materials.add(Color::srgb_u8(255, 0, 0)),
+        transform: Transform::from_xyz(0.0, 4.0, -1.0),
+        ..default()
+      });
 
-  // Sphere forward
-  commands.spawn(PbrBundle {
-    mesh: meshes.add(Sphere::new(SPHERE_SIZE)),
-    material: materials.add(Color::srgb_u8(255, 0, 0)),
-    transform: Transform::from_xyz(0.0, 4.0, -1.0),
-    ..default()
-  });
-
-  // PointLight
-  commands.spawn(PointLightBundle {
-    point_light: PointLight {
-      color: Color::srgb(1.0, 1.0, 0.8),
-      intensity: 600000.0,
-      range: 60.0,
-      shadows_enabled: true,
-      ..default()
-    },
-    transform: Transform::from_xyz(0.0, 8.0, 0.0),
-    ..default()
-  });
+      // PointLight
+      parent.spawn(PointLightBundle {
+        point_light: PointLight {
+          color: Color::srgb(1.0, 1.0, 0.8),
+          intensity: 600000.0,
+          range: 60.0,
+          shadows_enabled: true,
+          ..default()
+        },
+        transform: Transform::from_xyz(0.0, 8.0, 0.0),
+        ..default()
+      });
+    });
 }
