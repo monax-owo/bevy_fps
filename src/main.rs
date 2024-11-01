ef2227dee94c4e055cf5b74837ccbf75e01a80f8
zprl <zprl.dev@gmail.com>
2024-11-01 13:10:54 +0900

    create task

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 pub mod game;
 
+use std::env;
+
 use bevy::prelude::*;
 use game::plugin::GamePlugin;
 // use bevy_rapier3d::prelude::*;
@@ -8,8 +10,7 @@ fn main() {
   let mut app = App::new();
   app.add_plugins((DefaultPlugins, GamePlugin));
 
-  // 必要なときのみfalse
-  if true {
+  if env::args().find(|v| v == "--gui").is_some() {
     use bevy_editor_pls::EditorPlugin;
     app.add_plugins(EditorPlugin::default());
   }
