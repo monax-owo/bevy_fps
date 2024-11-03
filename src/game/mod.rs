8c18a484f5f866cd0e78e78ca5abe21cdcb166d7
zprl <zprl.dev@gmail.com>
2024-11-03 21:48:26 +0900

    create state.rs,todo

--- src/game/mod.rs
+++ src/game/mod.rs
@@ -1,6 +1,7 @@
 pub(super) mod player;
 pub(super) mod plugin;
 pub(super) mod shader;
+pub(super) mod state;
 pub(super) mod test;
 pub(super) mod ui;
 pub(super) mod world;
---
+++ src/game/state.rs
@@ -0,0 +1,7 @@
+// TODO:画面の状態を管理する
+pub(super) enum GameState {
+  MainMenu,
+  InGame,
+  // TODO:ポーズ画面
+  // PauseMenu,
+}
