e00f633b53980dac7e069147254dc383ae0a928e
zprl <zprl.dev@gmail.com>
2024-11-08 17:57:16 +0900

    up

--- src/game/shooting/weapons/test_gun.rs
+++ src/game/shooting/weapons/test_gun.rs
@@ -1,20 +1,29 @@
 use bevy::prelude::*;
 
+use crate::game::shooting::bullet::{Bullet, BulletAssets};
+
 #[derive(Component, Reflect, Debug, Default)]
 pub struct TestGun {
   pub cool_time: f32,
 }
 
-pub(super) fn update() {}
-
-// TODO:弾を発射
-// commands.entity(entity).with_children(|parent| {
-//   parent.spawn((
-//     PbrBundle {
-//       mesh: assets.bullet_mesh.clone(),
-//       material: assets.bullet_material.clone(),
-//       ..default()
-//     },
-//     Bullet { lifetime: 10.0 },
-//   ));
-// });
+pub(super) fn update(
+  mut commands: Commands,
+  assets: Res<BulletAssets>,
+  gun: Query<(Entity, &TestGun, &Transform)>,
+) {
+  for (entity, gun, transform) in gun.iter() {
+    println!("{}", transform.translation);
+    // TODO:弾を発射
+    // commands.entity(entity).with_children(|parent| {
+    //   parent.spawn((
+    //     PbrBundle {
+    //       mesh: assets.bullet_mesh.clone(),
+    //       material: assets.bullet_material.clone(),
+    //       ..default()
+    //     },
+    //     Bullet { lifetime: 10.0 },
+    //   ));
+    // });
+  }
+}
