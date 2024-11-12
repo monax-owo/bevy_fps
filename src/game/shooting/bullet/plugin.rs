49c12cd65ade26f761022e60d3cc3983887f0b08
zprl <zprl.dev@gmail.com>
2024-11-12 18:56:50 +0900

    up plugin

--- src/game/shooting/bullet/plugin.rs
+++ src/game/shooting/bullet/plugin.rs
@@ -4,7 +4,7 @@ use super::{
   projectile::{
     init_projectile_bullet, update_projectile_bullet, ProjectileBullet, ProjectileBulletAssets,
   },
-  raycast::{RaycastBullet, RaycastBulletAssets},
+  raycast::{init_raycast_bullet, update_raycast_bullet, RaycastBullet, RaycastBulletAssets},
 };
 
 pub struct BulletPlugin;
@@ -12,8 +12,8 @@ pub struct BulletPlugin;
 impl Plugin for BulletPlugin {
   fn build(&self, app: &mut App) {
     app
-      .add_systems(Startup, init_projectile_bullet)
-      .add_systems(Update, update_projectile_bullet)
+      .add_systems(Startup, (init_projectile_bullet, init_raycast_bullet))
+      .add_systems(Update, (update_projectile_bullet, update_raycast_bullet))
       .init_resource::<ProjectileBulletAssets>()
       .init_resource::<RaycastBulletAssets>()
       .register_type::<ProjectileBullet>()
