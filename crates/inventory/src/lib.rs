7a6f64cd0da2d56217a46b97efe94d6512c465ff
zprl <zprl.dev@gmail.com>
2024-11-15 19:42:09 +0900

    remove items

--- crates/inventory/src/lib.rs
+++ crates/inventory/src/lib.rs
@@ -4,53 +4,28 @@ pub enum InventoryError {
 }
 
 #[derive(Component, Reflect, Debug)]
-pub struct Inventory<T = Item> {
-  /// インベントリが保持しているアイテム
-  /// Noneは空のスロットを指す
-  pub items: Vec<Option<T>>,
-  // TODO:要らなくない？
+pub struct Inventory {
   /// itemsのindex
   pub current_item: usize,
   /// itemsの最大の長さ
   max_count: usize,
 }
 
-impl<T> Default for Inventory<T> {
+impl Default for Inventory {
   fn default() -> Self {
     Self {
-      items: Default::default(),
       current_item: Default::default(),
       max_count: 1,
     }
   }
 }
 
-impl Inventory<Item> {
+impl Inventory {
   pub fn new(max_count: usize) -> Self {
     Self {
       max_count,
       ..Default::default()
     }
-  }
-}
-
-// TODO
-impl<T> Inventory<T> {
-  /// itemをインベントリに追加する
-  /// # Errors
-  /// itemsの長さがmax_count以上の場合に`InventoryError::Overflow`を返す
-  pub fn push(&mut self, item: T) -> Result<(), InventoryError> {
-    if self.items.len() >= self.max_count {
-      return Err(InventoryError::Overflow);
-    }
-    self.items.push(Some(item));
-    Ok(())
-  }
-
-  //
-  pub fn equip(&mut self, _item: T) -> Result<T, InventoryError> {
-    todo!();
-    // Ok(item)
   }
 
   pub fn max_count(&self) -> usize {
