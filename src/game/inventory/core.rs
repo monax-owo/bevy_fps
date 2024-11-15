fc57c8c298f9b3d2429b782f8b718b79e3adb328
zprl <zprl.dev@gmail.com>
2024-11-15 22:42:10 +0900

    remove PlayerInventory

--- src/game/inventory/core.rs
+++ src/game/inventory/core.rs
@@ -1,40 +1,10 @@
-use std::any::Any;
-
 use bevy::prelude::*;
 use inventory::Inventory;
 
 use crate::game::player::input::PlayerInput;
 
-#[derive(Component, Reflect)]
-#[reflect(Default)]
-pub struct PlayerInventory {
-  /// current_itemのモデルを表示させるエンティティ
-  /// `None`の場合はこのコンポーネントを持っているエンティティを指す
-  pub item_user: Option<Entity>,
-  #[reflect(ignore)]
-  pub current_item_type: Box<dyn Any + Send + Sync + 'static>,
-}
-
 #[derive(Component, Reflect, Debug)]
 pub struct CurrentWeapon;
-
-impl Default for PlayerInventory {
-  fn default() -> Self {
-    Self {
-      item_user: Default::default(),
-      current_item_type: Box::new(()),
-    }
-  }
-}
-
-impl PlayerInventory {
-  pub fn new(item_user: Entity) -> Self {
-    Self {
-      item_user: Some(item_user),
-      ..Default::default()
-    }
-  }
-}
 
 pub(super) fn update_current_item(
   mut commands: Commands,
@@ -58,7 +28,7 @@ pub(super) fn update_current_item(
       .collect();
 
     // TODO: CurrentWeaponを変えれるようにする
-    let current_weapon = match find.len() {
+    let _current_weapon = match find.len() {
       1 => *find[0],
       0 => {
         dbg!("0");
--- src/game/inventory/plugin.rs
+++ src/game/inventory/plugin.rs
@@ -1,13 +1,11 @@
 use bevy::prelude::*;
 
-use super::{update_current_item, PlayerInventory};
+use super::update_current_item;
 
 pub struct InventoryPlugin;
 
 impl Plugin for InventoryPlugin {
   fn build(&self, app: &mut App) {
-    app
-      .add_systems(Update, update_current_item)
-      .register_type::<PlayerInventory>();
+    app.add_systems(Update, update_current_item);
   }
 }
