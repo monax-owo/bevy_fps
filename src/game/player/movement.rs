365243033b90089b0110604e4546d22c536a8456
zprl <zprl.dev@gmail.com>
2024-11-16 18:07:25 +0900

    up

--- src/game/player/movement.rs
+++ src/game/player/movement.rs
@@ -91,59 +91,6 @@ pub(super) fn update_movement_input(
 
 // Playerのプロパティを使用してエンティティを移動させる
 pub(super) fn update_movement(
-  keyboard_input: Res<ButtonInput<KeyCode>>,
-  key: Res<PlayerInput>,
-  time: Res<Time>,
-  mut player_query: Query<(
-    &mut Player,
-    &Transform,
-    &mut KinematicCharacterController,
-    &GroundSensor,
-  )>,
-) {
-  const JUMP_HEIGHT: f32 = -80.0;
-
-  if let Ok((mut player, player_transform, mut controller, ground_sensor)) =
-    player_query.get_single_mut()
-  {
-    let mut direction = Vec3::ZERO;
-
-    if keyboard_input.pressed(key.forward) {
-      direction.x += 1.0;
-    }
-
-    if keyboard_input.pressed(key.left) {
-      direction.z += -1.0;
-    }
-
-    if keyboard_input.pressed(key.back) {
-      direction.x += -1.0;
-    }
-
-    if keyboard_input.pressed(key.right) {
-      direction.z += 1.0;
-    }
-
-    // TODO:プレイヤーが止まったら歩きの速度にする
-    if keyboard_input.pressed(key.dash) {
-      player.horizontal_speed = 20.0;
-    }
-
-    direction = direction.x * player_transform.forward() + direction.z * player_transform.right();
-
-    // jump
-    if ground_sensor.grounded && keyboard_input.pressed(key.jump) {
-      player.vertical_accel += JUMP_HEIGHT;
-    }
-
-    player.direction =
-      (direction * player.horizontal_speed).with_y(player.direction.y) * time.delta_seconds();
-
-    controller.translation = Some(player.direction);
-  }
-}
-
-pub(super) fn update_gravity(
   time: Res<Time>,
   mut player_query: Query<(
     &mut Player,
@@ -153,22 +100,31 @@ pub(super) fn update_gravity(
   )>,
 ) {
   const GRAVITY: f32 = 9.8;
+  const JUMP_HEIGHT: f32 = -80.0;
 
   if let Ok((mut player, player_transform, mut controller, ground_sensor)) =
     player_query.get_single_mut()
   {
+    let mut direction = Vec3::ZERO;
+
+    direction = direction.x * player_transform.forward() + direction.z * player_transform.right();
+
     // 地面に付いて無いときは重力を加える
     if ground_sensor.grounded {
       player.vertical_accel = (player.vertical_accel
         - player.vertical_speed * 2.2 * time.delta_seconds())
       .clamp(9.8, 20.0);
+    // TODO: jump
     } else {
       player.vertical_accel = (player.vertical_accel
         + GRAVITY * player.vertical_speed * time.delta_seconds())
       .clamp(-500.0, 500.0);
     }
 
-    player.direction.y -= player.vertical_accel * 0.2 * time.delta_seconds();
+    player.direction.y -= player.vertical_accel * 0.2;
+
+    player.direction =
+      (direction * player.horizontal_speed).with_y(player.direction.y) * time.delta_seconds();
 
     controller.translation = Some(player.direction);
   }
