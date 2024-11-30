93c0ab8caec38c68414ec8b46a0d7e5e3dbebb08
zprl <zprl.dev@gmail.com>
2024-11-30 13:28:05 +0900

    add startup

--- src/game/plugin.rs
+++ src/game/plugin.rs
@@ -3,6 +3,7 @@ use bevy_rapier3d::{
   plugin::{NoUserData, RapierPhysicsPlugin},
   render::RapierDebugRenderPlugin,
 };
+use blenvy::{BlenvyPlugin, BluePrintBundle, BlueprintInfo};
 
 use super::{
   inventory::plugin::InventoryPlugin, player::PlayerPlugin, shader::ShaderPlugin,
@@ -13,21 +14,34 @@ pub struct GamePlugin;
 
 impl Plugin for GamePlugin {
   fn build(&self, app: &mut bevy::prelude::App) {
-    let app = app.add_plugins((
-      RapierPhysicsPlugin::<NoUserData>::default(),
-      // inventory crateのPlugin
-      inventory::InventoryPlugin,
-      InventoryPlugin,
-      PlayerPlugin,
-      ShaderPlugin,
-      ShootingPlugin,
-      StatePlugin,
-      UiPlugin,
-      WorldPlugin,
-    ));
+    let app = app
+      .add_plugins((
+        RapierPhysicsPlugin::<NoUserData>::default(),
+        BlenvyPlugin::default(),
+        // inventory crateのPlugin
+        inventory::InventoryPlugin,
+        InventoryPlugin,
+        PlayerPlugin,
+        ShaderPlugin,
+        ShootingPlugin,
+        StatePlugin,
+        UiPlugin,
+        WorldPlugin,
+      ))
+      .add_systems(Startup, setup_blenvy);
 
     if cfg!(debug_assertions) {
       app.add_plugins((TestPlugin, RapierDebugRenderPlugin::default()));
     }
   }
+}
+
+// blenvy
+fn setup_blenvy(mut commands: Commands) {
+  commands.spawn(
+    (BluePrintBundle {
+      blueprint: BlueprintInfo::from_path("levels.world.glb"),
+      ..default()
+    }),
+  )
 }
